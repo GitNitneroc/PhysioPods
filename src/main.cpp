a1cf108b0286c597120c8b67167e23164703e44e
Corentin <donttry@stfu.com>
2024-02-13 22:36:07 +0100

    Début d'une page html pour choisir le mode. Pour l'instant ça ne fait rien

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@
 #include "handlers/LEDRequestHandler.h"
 #include "handlers/CaptiveRequestHandler.h"
 #include "handlers/ServerMacAddressHandler.h"
+#include "handlers/ModeChoiceHandler.h"
 
 //Our control
 #include "controls/ButtonControl.h"
@@ -27,7 +28,7 @@
 DNSServer dnsServer;
 AsyncWebServer server(80);
 String html = String(
-#include "index.html"
+#include "./html/index.html"
 );
 
 ButtonControl* control = new ButtonControl(BUTTON_PIN);
@@ -52,9 +53,12 @@ void setup(){
     dnsServer.start(53, "*", WiFi.softAPIP());
 
     //handlers for the web server
-    //TODO : create a handler to start the mode
-    server.addHandler(new LEDRequestHandler(LED_PIN, &html)); //Handles the LED control requests
+    //TODO : create a handler to start/stop the mode
+    //TODO : create a handler to get the score
+    //TODO : create a handler where other physioPods can send their mac address and register themselves to the server, and get the server mac address, this will be used for ESPNow
+    server.addHandler(new ModeChoiceHandler()); //Handles the mode choice request
     server.addHandler(new ServerMacAddressHandler()); //Handles the server mac address request
+    server.addHandler(new LEDRequestHandler(LED_PIN, &html)); //Handles the LED control requests
     server.addHandler(new CaptiveRequestHandler(&html));//call last, if no specific handler matched
     
 
