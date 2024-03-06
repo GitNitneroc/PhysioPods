52517cdd4db51a7e7834f6d115a5fa9b85ba27b9
Corentin <donttry@stfu.com>
2024-03-06 15:12:58 +0100

    LED_PIN est maintenant un build-flag plutôt qu'un define

--- src/pins.h
+++ src/pins.h
@@ -1,8 +1,7 @@
 //This file contains the pin definitions (for the LED and the button)
 #ifndef PINS_H
 #define PINS_H
-#define LED_PIN 48 // 48 is the built-in LED on the ESP32-S3
-//#define LED_PIN 8 // 8 is the built-in LED on the ESP32-C3
+
 #define BUTTON_PIN 10
 
 
